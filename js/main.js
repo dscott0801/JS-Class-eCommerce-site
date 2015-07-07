9dd436b4103c7bf2600cc3ca9de4e176eebec1b8
dscott0801 <dscott0801@gmail.com>
2015-07-07 19:27:31 -0400

    big update in js, css, and index
    js: Added products. Researching how to use images as properties. Added
    main 5 products.
    
    css: Adjusted carousel and thumbnails. Added shopping cart to top with
    sample .cart-item. and .shopping-cart-collapse to collapse div.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 //main js file
 
-function Product(name, price, image) {
+function Product(name, price, image, description) {
 	this.name = name;
 	this.price = price;
 	this.image = ;
+	this.description = description
 }
 
 var products = [];
 
 var cart = [];
+
+
+var poloShirt = new Product ('Polo Shirt', 24.99, , 'Men\'s Polo Shirt');
+var sundress = new Product ('Sundress', 29.99, , 'Ladies\' Sundress');
+var shoes = new Product ('Shoes', 29.99, , 'Men\'s and Women\'s shoes');
+var blouse = new Product ('Blouse', 18.99, , 'Women\'s Blouse');
+var jeans = new Product ('Jeans', 29.99, , 'Men\'s and Women\'s shoes');
